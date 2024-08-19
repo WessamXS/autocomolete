33f5636f63f0a6bb5ba22293a87c8deadeefcde7
wessam Al-sasa <alblal2001@gmail.com>
2024-08-19 17:43:54 +0900

    upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { TypeAnimation } from 'react-type-animation';
 
 function App() {
+  const [data, setdata] = useState([]);
+  const [newdata, setnewdata] = useState([]);
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/users")
+      .then(res => {
+        setdata(res.data)
+        setnewdata(res.data)
+      })
+      .catch(err => console.log(err))
+  }, [])
+  const filter = (e) => {
+    setnewdata(data.filter(f => f.name.toLowerCase().includes(e.target.value)))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className="App ">
+
+    <TypeAnimation
+    sequence={[
+      'Search Bar',
+      1000,
+            () => {
+        console.log('Sequence completed');
+      },
+    ]}
+    wrapper="span"
+    cursor={true}
+    repeat={Infinity}
+    speed={10}
+    style={{ fontSize: '50px', display: 'inline-block',margin:'20px'}}
+  />
+      <div className='card'>
+        <div className='child'>
+          <input className="input" onChange={filter} placeholder='Search' />
+          <table>
+            <div className='table'>
+              <thead className='thead'>
+                <tr>
+                  <th>ID</th>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Phone</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                {newdata.map((d, i) => (
+                  <tr key={i}>
+                    <td>{d.id}</td>
+                    <td>{d.name}</td>
+                    <td>{d.email}</td>
+                    <td>{d.phone}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </div>
+          </table>
+          </div>
+      </div>
+  
+      </div>
+      );
+    }
 
 export default App;
